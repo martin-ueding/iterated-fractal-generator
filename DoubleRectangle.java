d3955d6ba43ffdf40b5489f1a1c97b2fe1b0b522
Martin Ueding <von.bzr@martin-ueding.de>
2010-10-08 20:15:58 +0200

    Copyright/License

--- DoubleRectangle.java
+++ DoubleRectangle.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2010 Martin Ueding <mu@martin-ueding.de>
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.awt.Graphics2D;
 import java.awt.Point;
 
@@ -6,14 +16,6 @@ import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-
-/**
- * Iteriertes Funktionssystem
- * 
- * Martin Ueding - www.martin-ueding.de
- * 
- * @author Martin Ueding
- */
 
 public class DoubleRectangle {
 	static int auto_increment = 1;
